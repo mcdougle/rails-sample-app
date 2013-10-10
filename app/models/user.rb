72e7cecc14a2f9b8457b1a45e83b5f2fb4b4e9bc
Anthony McDougle <anthony.mcdougle@mcdougle.net>
2013-10-10 16:18:14 -0400

    sign up complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 	
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 	validates :email,		presence: true,
-								format: { with: VALID_EMAIL_REGEX }
+								format: { with: VALID_EMAIL_REGEX },
 								uniqueness: { case_sensitive: false }
 	
 	validates :password,	length: { minimum: 6 }
