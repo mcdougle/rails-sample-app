656cb4dc08c387754f8daaf5538a47a80614138a
Anthony McDougle <anthony.mcdougle@mcdougle.net>
2013-10-12 10:28:03 -0400

    completed signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,11 @@ class UsersController < ApplicationController
 	end
 	
 	def create
+		@user = User.new(params[:user])
+		if @user.save
+			redirect_to @user, :notice => "successfully signed up!"
+		else
+			render 'new'
+		end
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	attr_accessible :email, :password_digest
+	attr_accessible :email, :password, :password_confirmation
 	has_secure_password
 	
 	before_save { self.email.downcase! }
