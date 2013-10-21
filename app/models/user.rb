192cba65d319163a4fdce86a07ab776d5df5b23b
Anthony McDougle <anthony.mcdougle@mcdougle.net>
2013-10-21 15:36:37 -0400

    finished tutorial

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,7 +39,7 @@ class User < ActiveRecord::Base
 	
 	### FEED STUFF
 	def feed
-		Micropost.where('user_id = ?', id)		# could just use `microposts` to get current user's list
+		Micropost.from_users_followed_by(self)
 	end
 	
 	### RELATIONSHIP STUFF
